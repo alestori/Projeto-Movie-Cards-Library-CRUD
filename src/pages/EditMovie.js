2ad9ba58433b56c5264896570df15ed8dbb36f35
Renan Ferreira <renansantiago2013@hotmail.com>
2021-07-06 18:39:45 -0300

    Resolve requisito 5 - requisiçao para o filme que sera renderizado

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      status: 'loading',
+      movie: [],
+      shouldRedirect: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handleSubmit(updatedMovie) {
-  // }
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const { getMovie } = movieAPI;
+    getMovie(id);
+    this.takeMovie();
+  }
+
+  handleSubmit(updatedMovie) {
+    const { createMovie } = movieAPI;
+    createMovie(updatedMovie);
+    this.setState({
+      movie: updatedMovie,
+      shouldRedirect: true,
+    });
+  }
+
+  async takeMovie() {
+    const { match: { params: { id } } } = this.props;
+    const requestResponse = await movieAPI.getMovie(id);
+    const selectedMovie = await requestResponse;
+    this.setState({
+      movie: selectedMovie,
+      status: 'loaded',
+    });
+  }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return (
+        <p>Carregando...</p>
+      );
     }
 
     return (
@@ -30,5 +61,9 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.objectOf(PropTypes.object).isRequired,
+};
 
 export default EditMovie;
