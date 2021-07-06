f614c13e1dbb5d1d7efea5f3a00746e587feafe8
José Carlos <kalarruda@hotmail.com>
2021-07-06 18:33:20 -0300

    requisito 2 sem erros de lint

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import { Loading } from '../components';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
+    this.apiResponse = this.apiResponse.bind(this);
+
     this.state = {
       movies: [],
+      loading: true, // acrescentei isso na monitoria
     };
   }
 
+  componentDidMount() {
+    this.apiResponse();
+  }
+
+  async apiResponse() {
+    const response = await movieAPI.getMovies();
+    this.setState({
+      movies: response,
+      loading: false,
+    });
+  }
+  // async componentDidMount() {
+  //   const reponse = await movieAPI.getMovies();
+  //   this.setState({
+  //     movies: reponse,
+  //     loading: false,
+  //   });
+  // }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
+    const map = movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { loading ? <Loading /> : map }
       </div>
     );
   }
