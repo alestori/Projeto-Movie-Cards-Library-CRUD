31b3ae959d45d49ee2f36b23d64c5cc5e2ce0a9c
Renan Ferreira <renansantiago2013@hotmail.com>
2021-07-06 15:47:46 -0300

    Resolve requisito 2 - requisiçao para buscar e mostrar a lista de filmes

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
+    this.takeMovies = this.takeMovies.bind(this);
+
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.takeMovies();
+  }
+
+  takeMovies() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const requestResponse = await movieAPI.getMovies();
+        const moviesObj = await requestResponse;
+        this.setState({
+          movies: moviesObj,
+          loading: false,
+        });
+      },
+    );
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading /> : movies.map((movie) => (<MovieCard
+          key={ movie.title }
+          movie={ movie }
+        />))}
       </div>
     );
   }
