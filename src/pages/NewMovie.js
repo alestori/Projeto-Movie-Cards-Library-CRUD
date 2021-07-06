8e93e61ec62553a3b09f8a22919d56267d33e67b
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-06 18:02:03 -0300

    conclui requisito_02 - Faça uma requisição para buscar e mostrar a lista de filmes quando MovieList for montado

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -6,7 +6,7 @@ import * as movieAPI from '../services/movieAPI';
 class NewMovie extends Component {
   constructor(props) {
     super(props);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
