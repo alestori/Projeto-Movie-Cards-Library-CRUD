61d4892a8819315997b9557c9f46622c0606a3da
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-07-06 18:44:52 -0300

    Correction and implamentation the proptyp for lint pass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
-import Routes from './routes';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Routes />
+      <BrowserRouter>
+        <div>Movie Card Library CRUD</div>
+        <Route
+          path="/"
+          exact
+          render={
+            (props) => <MovieList { ...props } />
+          }
+        />
+        <Route
+          path="/movies/:id"
+          exact
+          render={
+            (props) => <MovieDetails { ...props } />
+          }
+        />
+        <Route
+          path="/movies/new"
+          exact
+          render={
+            (props) => <NewMovie { ...props } />
+          }
+        />
+        <Route
+          path="/movies/:id/edit"
+          exact
+          render={
+            (props) => <EditMovie { ...props } />
+          }
+        />
+        <Route
+          path=""
+          component={ NotFound }
+        />
+      </BrowserRouter>
     );
   }
 }
