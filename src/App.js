a0c023f4c6d3cd326878b78c3f678a3067e69c68
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-07-06 15:21:40 -0300

    Iniciando o projeto do jeito XABLAU de ser!!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  // state = { }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/" component={ MovieList } />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
