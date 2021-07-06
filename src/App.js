5a9d6b042f7c36144ff9216405073824b041036d
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-07-06 16:50:22 -0300

    primeiro requisito ok, o lint esta com problema de que nao foi usado o movie api e outros

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
   // state = { }
@@ -8,7 +12,11 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={ MovieList } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="*" component={ NotFound } />
       </BrowserRouter>
     );
   }
