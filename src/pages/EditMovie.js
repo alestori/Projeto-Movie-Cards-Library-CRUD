cdae9c22a137e627ff44f21e5e13d57bacf76579
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-07-06 16:34:26 -0300

    component EditMovie done, update selected movie and redirect to home

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 
-import { MovieForm } from '../components';
+import { MovieForm, Loading } from '../components';
+import { Redirect } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      load: true,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie).then(() => this.setState({ shouldRedirect: true }));
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+
+    movieAPI
+      .getMovie(id)
+      .then((movie) => this.setState({ movie, load: false }));
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { load, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
-    if (status === 'loading') {
-      // render Loading
+    if (load) {
+      return <Loading />
     }
 
     return (
