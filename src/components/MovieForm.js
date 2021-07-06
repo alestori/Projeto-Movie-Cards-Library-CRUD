9291b57e616fcdeaa7e220f19c6404dcba25ee4e
Elias-Forte <eliasforte.tur@gmail.com>
2021-07-06 15:39:47 -0300

    iniciando projeto

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -164,4 +164,9 @@ class MovieForm extends React.Component {
   }
 }
 
+MovieForm.propType = {
+  movie: PropTypes.array.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
 export default MovieForm;
