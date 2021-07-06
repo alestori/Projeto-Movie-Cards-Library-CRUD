98fb7b28b621376cb51d9eb38df036b7754dc97d
pedro21medeiros <pedro21medeiros@gmail.com>
2021-07-06 15:34:45 -0300

    começa a construçao do MovieDetails, criada a funçao que deleta os filmes baseado no ID

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      movie: [],
+      loading: true,
+    };
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    movieAPI.getMovie(id)
+      .then((result) => this.setState({ movie: result, loading: false }));
+  }
+
+  handleDelete() {
+    const { match } = this.props;
+    const { id } = match.params;
+    movieAPI.deleteMovie(id);
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    const { loading, movie } = this.state;
 
     const { title, storyline, imagePath, genre, rating, subtitle } = {};
 
