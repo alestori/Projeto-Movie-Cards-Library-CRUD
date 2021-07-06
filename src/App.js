131e6ee8ea16b4baf7e9014b7d464982283fa872
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-06 14:53:54 -0300

    renderiza paginas no componente App e estabelece suas rotas (requisito 01)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/movies/new" render={ (props) => <NewMovie { ...props } /> } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route path="/:notfound" render={ (props) => <NotFound { ...props } /> } />
+        <Route path="/" render={ (props) => <MovieList { ...props } /> } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
