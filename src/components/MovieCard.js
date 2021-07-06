7cd0652070ee31ce707b0d314748f96ab4dbda33
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-06 14:09:02 -0300

    Requirement 03-> Render elements in `MovieCard`

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt={ title } />
+        <p>{ title }</p>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }> VER DETALHES </Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    bookmarked: PropTypes.bool.isRequired,
+    genre: PropTypes.string.isRequired,
+  }).isRequired).isRequired,
+};
 
 export default MovieCard;
