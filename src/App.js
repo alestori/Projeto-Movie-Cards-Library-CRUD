ee9b049a224c6d090f0c082acd3eb715112fb07b
Thiago Prado <thiagopc_dev@hotmail.com>
2021-07-06 20:08:42 -0300

    Desafio 4

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
         <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        {/* <Route path="/movies/:id" component={ MovieDetails } /> */}
         <Route path="*" component={ NotFound } />
       </Switch>
     </Router>
