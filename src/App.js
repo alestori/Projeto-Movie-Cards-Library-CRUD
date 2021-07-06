37ee5937f45405262b9938ce0957a6e1945cd756
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-06 15:13:08 -0400

    refatora requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import NewMovie from './pages/NewMovie';
 import NotFound from './pages/NotFound';
@@ -10,11 +10,13 @@ import MovieDetails from './pages/MovieDetails';
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={ MovieList } />
-      <Route exact path="/movies/:id" component={ MovieDetails } />
-      <Route exact path="/movies/new" component={ NewMovie } />
-      <Route exact path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="*" component={ NotFound } />
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/*" component={ NotFound } />
+      </Switch>
     </BrowserRouter>
   );
 }
