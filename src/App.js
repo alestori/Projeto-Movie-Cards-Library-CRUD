1806453c3213f49f0cfd40fabd7e3e9b1f09f517
Gabriel Takaki <gabitj@icloud.com>
2021-07-06 19:22:02 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -12,10 +12,20 @@ function App() {
     <Router>
       <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route path="/" exact component={ MovieList } />
-        <Route path="/movies/:id" exact component={ MovieDetails } />
-        <Route path="/movies/new" exact component={ NewMovie } />
-        <Route path="/movies/:id/edit" exact component={ EditMovie } />
+        <Route exact path="/" component={ MovieList } />
+        {/* <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } /> */}
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route
+          exact
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
+        <Route
+          exact
+          path="/movies/:id/edit"
+          render={ (props) => <EditMovie { ...props } /> }
+        />
         <Route component={ NotFound } />
       </Switch>
     </Router>
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: [],
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.saveData = this.saveData.bind(this);
   }
 
-  handleSubmit(/* updatedMovie */) {
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const { getMovie } = movieAPI;
+    getMovie(id);
+    this.saveData();
+  }
+
+  handleSubmit(updatedMovie) {
+    const { createMovie } = movieAPI;
+    createMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+      movie: updatedMovie,
+    });
+  }
+
+  async saveData() {
+    try {
+      const { match: { params: { id } } } = this.props;
+      const { getMovie } = movieAPI;
+      this.setState({
+        status: 'done',
+        movie: await getMovie(id),
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
@@ -30,5 +63,9 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.objectOf(Object).isRequired,
+};
 
 export default EditMovie;
