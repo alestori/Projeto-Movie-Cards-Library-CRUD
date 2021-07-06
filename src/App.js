adc09c4f34919250528a1173b07292794a7d1d96
Renan Ferreira <renansantiago2013@hotmail.com>
2021-07-06 14:49:02 -0300

    Resolve requisito 1 - cria as rotas da aplicaçao

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <BrowserRouter>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      </BrowserRouter>
+    </div>
   );
 }
 
