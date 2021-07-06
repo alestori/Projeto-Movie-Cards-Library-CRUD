cc4d5638b062cd86bdf5ddfa9ef01948fce63166
Abner <gedeaoabner@gmail.com>
2021-07-06 16:53:24 -0300

    Adiciona Loading enquanto espera MovieList carregar - quesito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import { getMovies } from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +9,37 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+    this.callMovieCard = this.callMovieCard.bind(this);
+  }
+
+  componentDidMount() {
+    getMovies()
+      .then((json) => this.setState({
+        movies: json,
+      }))
+      .then(() => this.setState({
+        loading: false,
+      }));
+  }
+
+  callMovieCard() {
+    const { movies } = this.state;
+    return (
+      movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+    );
   }
 
   render() {
-    const { movies } = this.state;
+    const { loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {/* {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)} */}
+        { loading ? <Loading /> : this.callMovieCard() }
       </div>
     );
   }
