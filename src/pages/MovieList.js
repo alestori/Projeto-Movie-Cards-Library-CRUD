05ca7bfcc2026812d90e34fed24bd87919324cd1
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-07-06 17:01:36 -0300

    Requiriment 02

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
+
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    const response = await movieAPI.getMovies();
+    this.setState({
+      loading: false,
+      movies: response,
+    });
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="movie-list">
