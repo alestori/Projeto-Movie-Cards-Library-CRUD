b8aa5b0c46b45f514f7daeafea222ba4040275fa
Patrick Dack <patrickdack89@gmail.com>
2021-07-06 16:16:09 -0300

    Requisito 2 - Faz requisiçao a API e exibe mensagem carregando.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,19 +9,33 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  async fetchAPI() {
+    this.setState({ loading: true },
+      async () => {
+        const request = await movieAPI.getMovies();
+        this.setState({
+          movies: request,
+          loading: false,
+        });
+      });
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
-    return (
+    const { movies, loading } = this.state;
+    const card = (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
-    );
+      </div>);
+
+    return loading ? <Loading /> : card;
   }
 }
 
