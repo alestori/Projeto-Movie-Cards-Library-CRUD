65002612f97d55622bca9bdb7e4b90d4a3a952d6
Ramon Paschoal <rpaschoal90@gmail.com>
2021-07-06 19:23:02 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filme: {},
+      isLoading: true,
+
+    };
+  }
+
+  async componentDidMount() {
+    this.handleState1();
+  }
+
+  handleState1 = async () => {
+    const { match: { params: { id } } } = this.props;
+
+    const getFunction1 = await movieAPI.getMovie(id);
+    this.setState({
+      filme: { ...getFunction1 },
+      isLoading: false,
+    });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    const { match: { params: { id } } } = this.props;
+    const { filme, isLoading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = filme;
 
-    const { /* title, */ storyline, imagePath, genre, rating, subtitle } = {};
+    if (isLoading) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
       </div>
     );
   }
 }
 
 export default MovieDetails;
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    }),
+  }).isRequired,
+};
