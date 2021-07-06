a782297fa710cdd3a52179de4eeff9cde0ba5f13
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-07-06 15:38:07 -0300

    Requisito 1

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,14 @@
+import React from 'react';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <h2>Index de filmes</h2>
+    );
+  }
+}
+
+export default Index;
 export { default as Loading } from './Loading';
 export { default as MovieForm } from './MovieForm';
 export { default as MovieCard } from './MovieCard';
