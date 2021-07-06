7bb9d3ada1ff8a5eead02717425a1280c5a38cd2
Patrick Dack <patrickdack89@gmail.com>
2021-07-06 15:35:08 -0300

    Requisito 1 - Criada as rotas, exceto para a pagina NotFound

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Movie Card Library CRUD</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={ MovieList } />
+            <Route path="/movies/:id" component={ MovieDetails } />
+            <Route path="/movies/new" component={ NewMovie } />
+            <Route path="/movies/:id/edit" component={ EditMovie } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
