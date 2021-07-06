be359ef060b324831845d35cfe5c0180e9253b8b
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-07-06 15:06:57 -0300

    add route to all pages in project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>Movie Card Library CRUD</div>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="movies/:id/edit" component={EditMovie} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
