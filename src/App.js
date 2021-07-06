dbdef6c783e41cfffae8e26bfde1078a792c2f50
Kevin Oliveira <kevin.zero@hotmail.com>
2021-07-06 16:59:08 -0300

    (4) - Renderiza detalhes do filme em MovieDetails

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route
+          path="/movies/:id/edit"
+          render={ (routeProps) => <EditMovie { ...routeProps } /> }
+        />
         <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/" exact component={ MovieList } />
         <Route component={ NotFound } />
