6344ced5e6c3074311c0d244cde258d8ec8854f9
Merlone <alestori7@gmail.com>
2021-07-06 16:26:38 -0300

    [5] - requisito 4: renderiza MovieDetails ao clicar no link

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+    this.handleFetchMovie = this.handleFetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleFetchMovie();
+  }
+
+  async handleFetchMovie() {
+    const { match: { params: { id } } } = this.props;
+    this.setState({ loading: true },
+      async () => {
+        const movie = await movieAPI.getMovie(id);
+        const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+        this.setState({
+          title, storyline, imagePath, genre, rating, subtitle, id, loading: false,
+        });
+      });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    const { loading } = this.state;
+    if (loading) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = this.state;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h1>{`title: ${title}`}</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
+        <Link to="/">DELETAR</Link>
       </div>
     );
   }
