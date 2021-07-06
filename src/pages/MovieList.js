006186c95c41132f783b982eb7bc97ad686e12c1
RenanMiyashita <renanmiyashita@gmail.com>
2021-07-06 17:36:12 -0300

    requisitos 1 e 2 concluidos

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,20 +10,46 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      load: true,
     };
+
+    this.getMovies = this.getMovies.bind(this);
+    this.renderMovieCard = this.renderMovieCard.bind(this);
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.getMovies();
+  }
 
-    // Render Loading here if the request is still happening
+  async getMovies() {
+    const movies = await movieAPI.getMovies();
+    this.setState({
+      movies,
+      load: false,
+    });
+  }
+
+  renderMovieCard() {
+    const { movies } = this.state;
 
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+      </div>
+    );
+  }
+
+  render() {
+    const { load } = this.state;
+    const loadingComponent = <Loading />;
+    const movieCardComponent = this.renderMovieCard();
+
+    return (
+      <div>
+        { load ? loadingComponent : movieCardComponent }
       </div>
     );
   }
 }
-
 export default MovieList;
