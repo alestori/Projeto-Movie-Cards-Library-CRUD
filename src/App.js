55ee4d9537eef290e0cd5d788499833cbc50ee8d
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-07-06 16:07:45 -0300

    Corrigido linter novamente

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
         <Route path="/movies/:id" component={ MovieDetails } />
         <Route exact path="/" component={ MovieList } />
         <Route component={ NotFound } />
