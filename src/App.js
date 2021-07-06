04b9178c1f256340bf63f2e7210c3b633314fdb6
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-07-06 16:37:27 -0300

    render BrowserRouter in componet App using routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>Movie Card Library CRUD</div>
-      </Router>
+      <Routes />
     );
   }
 }
