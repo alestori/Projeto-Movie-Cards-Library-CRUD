1f58e5b4e4900a6c594ccd630380631c1d95065e
Nuwanda <morato_dev@protonmail.com>
2021-07-06 15:57:01 -0300

    pagina de ediçao de filmes feita

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      movie: [],
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const { getMovie } = movieAPI;
+    getMovie(id);
+    this.saveMovie();
+  }
+
   handleSubmit(updatedMovie) {
+    const { createMovie } = movieAPI;
+    createMovie(updatedMovie);
+    this.setState({
+      movie: updatedMovie,
+      shouldRedirect: true,
+    });
+  }
+
+  async saveMovie() {
+    const { match: { params: { id } } } = this.props;
+    const { getMovie } = movieAPI;
+    const selectedMovie = await getMovie(id);
+    this.setState({
+      status: 'loaded',
+      movie: selectedMovie,
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <span>Carregando...</span>;
     }
 
     return (
@@ -30,5 +57,9 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.objectOf(Object).isRequired,
+};
 
 export default EditMovie;
