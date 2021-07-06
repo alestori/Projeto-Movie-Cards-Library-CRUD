d4564c2964ea83b96f487e04408f03b8064001eb
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-07-06 16:06:58 -0300

    removi algumas classes que tinha criado, depois faço o estilo

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,11 +8,11 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, id } = movie;
     return (
       <div className="movie-card" data-testid="movie-card">
-        <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
-        <div className="movie-card-body">
-          <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <p className="movie-card-storyline">{storyline}</p>
+        <img alt="Movie Cover" src={ imagePath } />
+        <div>
+          <h4 data-testid="movie-card-title">{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
         </div>
         <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
