d0452c6d43455c5464de757e2c7dc42ed56a16e3
pedro21medeiros <pedro21medeiros@gmail.com>
2021-07-06 15:44:49 -0300

    emtrega requistio 4 funcionando, com botao editar(nao funcionando), voltar(funcionando) e detetar (deletando o filme ao clicar). Faltando PropTypes

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -7,7 +7,6 @@ import { Loading } from '../components';
 class MovieDetails extends Component {
   constructor(props) {
     super(props);
-    
     this.state = {
       movie: [],
       loading: true,
@@ -32,16 +31,27 @@ class MovieDetails extends Component {
     // Change the condition to check the state
     // if (true) return <Loading />;
     const { loading, movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { match } = this.props;
+    const { id } = match.params;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (loading) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <nav>
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          <Link to="/">VOLTAR</Link>
+          <Link to="/" onClick={ this.handleDelete }>DELETAR</Link>
+        </nav>
       </div>
     );
   }
