87181d7fb5156623a874b0da73e4bfa381dcb195
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-07-06 17:47:19 -0300

    Requisito 5 - Cria funcionalidade para editar informaçoes do filme

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      status: 'loading',
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit() {
+  componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    movieAPI.getMovie(id).then((movie) => {
+      this.setState({
+        movie,
+        status: 'loaded',
+      });
+    });
+  }
+
+  handleSubmit(movie) {
+    movieAPI.updateMovie(movie);
+    this.setState({
+      shouldRedirect: true,
+    });
   }
   // updatedMovie
 
@@ -18,10 +43,12 @@ class EditMovie extends Component {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
       // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
       // render Loading
+      return <Loading />;
     }
 
     return (
@@ -31,5 +58,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default EditMovie;
