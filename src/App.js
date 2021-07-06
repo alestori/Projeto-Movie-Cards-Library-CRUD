50c8ce5d9e4c32c98ccd39a62917889cca29aa71
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-07-06 15:30:40 -0300

    Retornei o estado original do projeto contendo os erros de lint, e vou fazendo passo a passo os requisitos e testando localmente. Requisito 2 testado e aprovado localmente

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
-      <Route path="/" component={ MovieList } />
-      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route exact path="/" component={ MovieList } />
       <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id" component={ MovieDetails } />
       <Route path="/movies/:id/edit" component={ EditMovie } />
       <Route component={ NotFound } />
     </Router>
