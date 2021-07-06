33a76b5e3f29435c3746de31854c2328ac1a5a9f
Eric Kreis <erickreis793@gmail.com>
2021-07-06 14:56:36 -0300

    Requisito 05

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -164,4 +164,18 @@ class MovieForm extends React.Component {
   }
 }
 
+MovieForm.defaultProps = {
+  movie: {},
+};
+
+MovieForm.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }),
+  onSubmit: PropTypes.func.isRequired,
+};
+
 export default MovieForm;
