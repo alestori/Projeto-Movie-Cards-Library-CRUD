50f1b5fd5a756a3f82c02d976cb6ca0a0e3f6f3c
pedro21medeiros <pedro21medeiros@gmail.com>
2021-07-06 14:46:27 -0300

    entrega requisito 1 - renderizar BrowserRouter no App.js usando rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/" exact component={ MovieList } />
+        <Route path="/movies/new" exact component={ NewMovie } />
+        <Route path="/movies/:id" exact component={ MovieDetails } />
+        <Route path="/movies/:id/edit" exact component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
