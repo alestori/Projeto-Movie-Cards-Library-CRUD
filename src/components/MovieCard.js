bbdb1779c14a69c8f1b79566a1c47766843d49cf
Patrick Dack <patrickdack89@gmail.com>
2021-07-06 17:00:26 -0300

    Requisito 3 - Criado um Link para o Id do filme

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h3>{ movie.title }</h3>
+        <p>{ movie.storyline }</p>
+        <p><Link to={ `/movies/${movie.id}` }>VER DETALHES</Link></p>
       </div>
     );
   }
