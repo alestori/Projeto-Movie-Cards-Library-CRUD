3430253453102c214b706c5437571db40380842d
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-06 14:53:19 -0300

    adicionando BrowserROuter e Routes ao componente App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
+import EditMovie from './pages/EditMovie';
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>Movie Card Library CRUD</div>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ MovieList } />
+            <Route exact path="/movies/:id" component={ MovieDetails } />
+            <Route exact path="/movies/new" component={ NewMovie } />
+            <Route exact path="/movies/:id/edit" component={ EditMovie } />
+            <Route component = { NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
