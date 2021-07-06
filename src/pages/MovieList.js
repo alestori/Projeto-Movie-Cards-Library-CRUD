2a01843d370f444a44ffe5ee469acd97bb2d4629
Roberval Filho <rslfilho@gmail.com>
2021-07-06 15:15:01 -0300

    Buscando filmes na API e rederizando Loading enquanto isso

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -12,14 +13,28 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    this.getMoviesFromApi();
+  }
+
+  async getMoviesFromApi() {
+    const response = await movieAPI.getMovies();
+
+    this.setState((prevState) => ({
+      movies: [...prevState.movies, ...response],
+    }));
+  }
+
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {
+          movies.length >= 1
+            ? movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+            : <Loading />
+        }
       </div>
     );
   }
