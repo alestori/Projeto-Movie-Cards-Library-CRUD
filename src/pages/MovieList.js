8f3ddd7eb375d24bc422e96712af8c500c0071b5
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-06 15:17:44 -0300

    cria funçaõ para renderizar a lista de filmes no componente MovieList e se a lista ainda não estiver sido carregada renderiza o componente Loading e carrega a lista assim que ela for carregada

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -10,12 +11,22 @@ class MovieList extends Component {
     this.state = {
       movies: [],
     };
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    const movies = await movieAPI.getMovies();
+    this.setState((state) => ({ ...state, movies: [...movies] }));
   }
 
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
+    if (!movies.length) return (<Loading />);
 
     return (
       <div data-testid="movie-list">
