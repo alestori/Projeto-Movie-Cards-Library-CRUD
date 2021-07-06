4deab9aa3855436148c7b8fd42f5ee9aa52a148b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-07-06 16:31:41 -0300

    Requiriment 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD!</div>
-  );
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
+import EditMovie from './pages/EditMovie';
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
+          <Route component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
