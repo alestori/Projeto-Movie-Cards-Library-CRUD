9eea9292769a1cab375661ac1a1c59dd190d891e
Erick Silva <erick.silva6653@gmail.com>
2021-07-06 15:11:09 -0300

    requisito 2

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -9,16 +9,27 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.setMovies();
+  }
+
+  setMovies = async () => {
+    const moviesArray = await movieAPI.getMovies();
+    await this.setState({ loading: false, movies: moviesArray });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
+        { loading && <p>Carregando...</p> }
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
