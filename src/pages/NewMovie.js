7b70a2b92db7081d8993ba00f2acb310c5a1c4aa
RyanLaiber <ryanlaiber@gmail.com>
2021-07-06 19:07:49 -0300

    adiciona requisito 6

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
@@ -7,12 +8,29 @@ class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      sholdRedirect: false,
+    };
   }
 
   handleSubmit(newMovie) {
+    this.fetchNewMovie(newMovie);
+    this.setState({
+      sholdRedirect: true,
+    });
+  }
+
+  async fetchNewMovie(newMovie) {
+    await movieAPI.createMovie(newMovie);
   }
 
   render() {
+    const { sholdRedirect } = this.state;
+
+    if (sholdRedirect) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
