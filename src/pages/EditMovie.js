63e13f213229805c726f440f14a56ddbafb1dbe2
Adryan Rosa <adryanrosa374@gmail.com>
2021-07-06 17:51:54 -0300

    Satisfaz requisito 5 - a fazer props validation de mtach e params ?????

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
+import { Loading, MovieForm } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      movie: {},
+      status: 'loading',
+      shouldRedirect: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
   }
 
-  // handleSubmit(updatedMovie) {
-  // }
-  handleSubmit() {
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
+  }
+
+  async fetchMovie() {
+    // eslint-disable-next-line react/prop-types
+    const { match: { params: { id } } } = this.props;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      movie,
+      status: 'loaded',
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
