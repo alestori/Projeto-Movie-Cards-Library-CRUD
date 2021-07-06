70f7ec016af3359e863885e8ec31454703e089cd
Marcela Silva <marcela.ops@hotmail.br>
2021-07-06 19:03:43 -0300

    req3 MovieCard com conteúdos do filme e Link

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { imagePath, title, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <img alt={ imagePath } src={ imagePath } />
+        <h4>{ title }</h4>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    // subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    // rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    imagePath: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
