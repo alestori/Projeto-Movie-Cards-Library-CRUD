52a47dee47cef1847be90d14f9c21688be5435e7
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-07-06 15:04:04 -0300

    innitializing the project.lets Go with all :rocket:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>Movie Card Library CRUD</div>
+      </Router>
+    );
+  }
 }
 
 export default App;
