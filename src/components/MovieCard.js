83fc511eaafd8f8db8399b6967cdb55c68807834
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-06 15:47:01 -0300

    adicionando css aos moviecards e terminando seu conteudo

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import './MovieCard.css';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
       <div data-testid="movie-card" className="movie-card">
-        <img />
-        <div>
+        <img src={ movie.imagePath} alt={ `${movie.title} cover`}/>
+        <div className="movie-card-text">
           <h4>{ movie.title }</h4>
-          <h5> { movie.subtitle }</h5>
           <p> { movie.storyline }</p>
         </div>
-        
+        <Link className="movie-card-details" to={ `/movies/${movie.id}` }>Mais Detalhes!</Link>
+                
       </div>
     );
   }
