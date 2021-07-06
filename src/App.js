fdc6b2606a922222197befbc7bc985db2e293151
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-06 16:51:17 -0300

    arrumando lint componentes App e MovieDetails

--- src/App.js
+++ src/App.js
@@ -16,7 +16,11 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={ MovieList } />
             <Route exact path="/movies/new" component={ NewMovie } />
-            <Route exact path="/movies/:id" render={ (props) => <MovieDetails {...props} /> } />
+            <Route
+              exact
+              path="/movies/:id"
+              render={ (props) => <MovieDetails { ...props } /> }
+            />
             <Route exact path="/movies/:id/edit" component={ EditMovie } />
             <Route component={ NotFound } />
           </Switch>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 import './MovieDetails.css';
@@ -13,7 +14,7 @@ class MovieDetails extends Component {
     this.state = {
       movie: {},
       loading: true,
-    }
+    };
   }
 
   componentDidMount() {
@@ -21,19 +22,19 @@ class MovieDetails extends Component {
   }
 
   async settingMovieState() {
-    const { match:{ params: { id } }} = this.props;
+    const { match: { params: { id } } } = this.props;
     const movie = await movieAPI.getMovie(id);
     this.setState({
       movie,
       loading: false,
-    })
+    });
   }
 
   render() {
     const { movie, loading } = this.state;
     const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
-    if(loading) return <Loading />
+    if (loading) return <Loading />;
     return (
       <div data-testid="movie-details" className="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
@@ -42,11 +43,19 @@ class MovieDetails extends Component {
         <p>{ `Sinopse: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
-        <Link className="movie-details-link" to={ `/movies/${id}/edit`}>EDITAR</Link>
+        <Link className="movie-details-link" to={ `/movies/${id}/edit` }>EDITAR</Link>
         <Link className="movie-details-link" to="/">VOLTAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
