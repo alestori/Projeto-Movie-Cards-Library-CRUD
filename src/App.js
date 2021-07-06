f7ece6cdc60870b7f5d5594784bbcf6ef0dcc371
matheus oliveira de proenca <mproenca.marketing@gmail.com>
2021-07-06 15:47:08 -0300

    Criando rotas - 01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <section>
       <div>Movie Card Library CRUD</div>
-    </Router>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/"><MovieList /></Route>
+          <Route path="/movies/new"><NewMovie /></Route>
+          <Route path="/movies/:id/edit"><EditMovie /></Route>
+          <Route path="/movies/:id"><MovieDetails /></Route>
+          <Route><NotFound /></Route>
+        </Switch>
+      </BrowserRouter>
+    </section>
   );
 }
 
 export default App;
-
