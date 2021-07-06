63cbb8c0eb325610ebed9157b7b778b0f4ed295f
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-06 18:49:59 -0300

    Adiciona link Adicionar Cartão em MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -30,6 +30,7 @@ class MovieList extends Component {
         {loading && <Loading />}
         <div data-testid="movie-list">
           {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+          <Link to="/movies/new">ADICIONAR CARTÃO</Link>
         </div>
       </>
     );
