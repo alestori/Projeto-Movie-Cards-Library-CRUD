e0b10374a8b11bf86a29d8c41f813f655747fec4
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-07-06 16:41:26 -0300

    Envia requisito 4 testado e aprovado localmente com referencia externas de outros codigos e explicaçoes

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: '',
+    };
+  }
+
+  componentDidMount() {
+    // https://github.com/tryber/sd-011-project-movie-card-library-crud/pull/116/commits/a08fa4556b3b574a4770a58b1aa52bccf5115641
+    // https://scotch.io/courses/using-react-router-4/route-params
+    const { match: { params: { id } } } = this.props;
+    movieAPI.getMovie(id).then((result) => this.setState({ movie: result }));
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
+    if (!movie) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
