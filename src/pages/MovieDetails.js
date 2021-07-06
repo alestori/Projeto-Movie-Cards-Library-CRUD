a116c04c8d064885062a164d1bcc01e435ccc683
Eduardo Linhares <modsgtasagame@gmail.com>
2021-07-06 17:39:47 -0300

    Resolve requisitos 4 e 7

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,21 +2,54 @@ import React, { Component } from 'react';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link, Redirect } from 'react-router-dom';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    movieAPI.getMovie(id).then((data) => {
+      this.setState({
+        loading: false,
+        movie: data,
+      });
+    })
+  }
+
+  handleClick = () => {
+    const { movie: { id } } = this.state;
+    movieAPI.deleteMovie(id).then(() =>
+      this.setState({
+        redirect: true
+      })
+    );
+  }
+
+  render() {
+    const { movie: { title, storyline, genre, imagePath, rating, subtitle, id },
+    loading, redirect } = this.state;
+
+    if (loading) return <Loading />;
+    if (redirect) return <Redirect to="/" />;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link onClick={ this.handleClick } to="/">DELETAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
