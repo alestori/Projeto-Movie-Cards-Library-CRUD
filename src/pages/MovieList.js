49176afc495acb60aeac5dc3500f11196a0e404d
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-07-06 15:45:12 -0300

    Requisito 2 - Cria requisiçao para API de filmes e exibe a mensagem de loading durante a requisiçao

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +10,31 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      moviesRequest: false,
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then((movies) => {
+      this.setState({
+        movies,
+        moviesRequest: true,
+      });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, moviesRequest } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {moviesRequest ? (
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        ) : (
+          <Loading />
+        )}
       </div>
     );
   }
