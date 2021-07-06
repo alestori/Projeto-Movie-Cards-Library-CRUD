ea7e9d6b3ff9e7a31e33a2db1194d128e8de218d
Renan Ferreira <renansantiago2013@hotmail.com>
2021-07-06 15:03:10 -0300

    Resolve requisito 1 - cria as rotas da aplicaçao

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route exact path="/movies/new" component={ NewMovie } />
         <Route exact path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="*" component={ NotFound } />
+        <Route path={ `/${Math.random()}` } component={ NotFound } />
       </BrowserRouter>
     </div>
   );
