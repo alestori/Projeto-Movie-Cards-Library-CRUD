0733d8f8e2e79d3eee00f356bed75dc5eddeb5c7
Pedro Gonçalves <peg.gep27@gmail.com>
2021-07-06 19:24:20 -0300

    requisitos 5 e 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -31,7 +32,12 @@ class MovieList extends Component {
     if (loading) return <Loading />;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <div>
+          {movies.map((movie) => (
+            <MovieCard key={ movie.title } movie={ movie } />
+          ))}
+        </div>
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
