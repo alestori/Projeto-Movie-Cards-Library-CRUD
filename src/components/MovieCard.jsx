7380490cd2a884d36d778de6d894c5792e8021f7
Erick Silva <erick.silva6653@gmail.com>
2021-07-06 16:09:42 -0300

    requisitos 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <div className="thumbnail">
+          <img src={ movie.imagePath } alt="Movie thumbnail" />
+          <h1>{ movie.title }</h1>
+        </div>
+        <p>{ movie.storyline }</p>
+        <Link to={ `movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true, movie: '' };
+  }
+
+  componentDidMount() {
+    this.setMovie();
+  }
+
+  setMovie = async () => {
+    const { match: { params: { id } } } = this.props;
+    const getMovie = await movieAPI.getMovie(id);
+    this.setState({ loading: false, movie: getMovie });
+  }
+
   render() {
     // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const { loading, movie } = this.state;
+    const { match: { params: { id } } } = this.props;
+    if (loading) { return <Loading />; }
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        {/* { loading && <Loading /> } */}
+        <img alt="Movie Cover" src={ `../${movie.imagePath}` } />
+        <h1>{ `Título: ${movie.title}` }</h1>
+        <p>{ `Subtitle: ${movie.subtitle}` }</p>
+        <p>{ `Storyline: ${movie.storyline}` }</p>
+        <p>{ `Genre: ${movie.genre}` }</p>
+        <p>{ `Rating: ${movie.rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
