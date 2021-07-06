5cdf4867490a25ffd85c4932929fb8bf96b95279
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-07-06 15:37:59 -0300

    render the movielis with moviecards and the Link button for details

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { id, title, storyline, imagePath } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={imagePath} alt={`${title} image`} />
+        <h3>{title} </h3>
+        <p>{storyline} </p>
+        <Link to={`/movies/${id}`}>Details</Link>
       </div>
     );
   }
