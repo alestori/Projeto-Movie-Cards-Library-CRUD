bfb0ff21b4bd76fe9a839a22921d1b55c582cd61
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-06 15:28:22 -0300

    arrumando os Routes do APP e iniciando Movie Card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div data-testid="movie-card" className="movie-card">
+        <img />
+        <div>
+          <h4>{ movie.title }</h4>
+          <h5> { movie.subtitle }</h5>
+          <p> { movie.storyline }</p>
+        </div>
+        
       </div>
     );
   }
