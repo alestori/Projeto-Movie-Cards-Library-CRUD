5d63a11e2f56dbff956e9fb3501ed4552e3fcc81
netosilv4 <netosilv44445@gmail.com>
2021-07-06 17:15:12 -0300

    Arrumando proptypes

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Loading from '../components/Loading';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
@@ -57,5 +58,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;
