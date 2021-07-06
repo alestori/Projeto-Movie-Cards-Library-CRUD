d3e7406210518562199d55f117610d31ec99def2
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-06 16:42:55 -0300

    Requisito 4 - Feito requisicao em MovieDetails e mostrado conteudo ao retornar, criado botao de voltar e editar

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    this.updateMovies(id);
+  }
+
+  updateMovies = async (id) => {
+    const response = await movieAPI.getMovie(id);
+    this.setState({
+      movie: response,
+      loading: false,
+    });
+  }
+
   render() {
     // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { loading } = this.state;
+    if (loading) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie } = this.state;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h1>{ `Subtitle: ${title}` }</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <button type="button"><Link to="/">VOLTAR</Link></button>
+        <button type="button"><Link to={ `/movies/${id}/edit` }>EDITAR</Link></button>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
