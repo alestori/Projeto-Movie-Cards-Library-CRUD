ce37cb35db6f1feea54442b383a026194b5795e9
Josimar Souza <contact.josimarsouza@gmail.com>
2021-07-06 16:54:18 -0300

    Implementa o componente MovieList com requisiçao dos filmes e mensagem de carregando e adiciona Switch no componente App

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,16 +9,25 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    const movies = movieAPI.getMovies();
+    movies.then((moviesArray) => {
+      this.setState({ movies: moviesArray });
+      this.setState({ loading: false });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
+        { loading ? <Loading /> : null }
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
