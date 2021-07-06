be3c770456fe95d9b3c15bc650b939415daff937
RyanLaiber <ryanlaiber@gmail.com>
2021-07-06 15:35:11 -0300

    adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
+      </Switch>
+      <div>Movie Card Library CRUD</div>
+    </BrowserRouter>
   );
 }
 
