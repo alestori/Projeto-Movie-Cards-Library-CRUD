33d26019e177030399931f068ef9c4bff12c8d01
Patrick Dack <patrickdack89@gmail.com>
2021-07-07 01:02:28 -0300

    Requisito 5 corrigido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -19,7 +19,6 @@ class App extends React.Component {
             <Route path="/movies/:id" component={ MovieDetails } />
             <Route component={ NotFound } />
           </Switch>
-          <button type="button"><Link to="/movies/new">ADICIONAR CARTÃO</Link></button>
         </BrowserRouter>
       </div>
     );
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -33,6 +34,7 @@ class MovieList extends Component {
     const card = (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <button type="button"><Link to="/movies/new">ADICIONAR CARTÃO</Link></button>
       </div>);
 
     return loading ? <Loading /> : card;
