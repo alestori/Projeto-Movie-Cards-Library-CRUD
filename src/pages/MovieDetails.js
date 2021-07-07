809d86fb2ed7920fe534c3888bfff00b7ee73989
Armando <a.guerra58@hotmail.com>
2021-07-06 21:22:32 -0300

    requisito 7

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import Loading from '../components/Loading';
@@ -8,9 +8,11 @@ class MovieDetails extends Component {
   constructor(props) {
     super(props);
     this.List = this.List.bind(this);
+    this.Delet = this.Delet.bind(this);
     this.state = {
       movie: {},
       loading: true,
+      shouldRedirect: false,
     };
   }
 
@@ -28,12 +30,19 @@ class MovieDetails extends Component {
     this.setState({ loading: false, movie: rende });
   }
 
+  async Delet() {
+    const { match: { params: { id } } } = this.props;
+    await movieAPI.deleteMovie(id);
+    this.setState({
+      shouldRedirect: true,
+    });
+  }
+
   render() {
-    const { movie, loading } = this.state;
+    const { movie, loading, shouldRedirect } = this.state;
     const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
-
+    if (shouldRedirect) return <Redirect to="/" />;
     if (loading) return <Loading />;
-
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
@@ -48,6 +57,7 @@ class MovieDetails extends Component {
           <br />
           <Link to="/">VOLTAR</Link>
         </div>
+        <Link to="/" onClick={ this.Delet }>DELETAR</Link>
       </div>
     );
   }
