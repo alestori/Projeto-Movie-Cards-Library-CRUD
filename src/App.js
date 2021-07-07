d82c6e51bace9751188c1789d4b06fb349dac9e1
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-06 23:11:33 -0300

    renderiza componente Header, cria e renderiza componente Footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route path="/movies/new" render={ (props) => <NewMovie { ...props } /> } />
         <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
@@ -16,6 +19,7 @@ function App() {
         <Route path="/:notfound" render={ (props) => <NotFound { ...props } /> } />
         <Route path="/" render={ (props) => <MovieList { ...props } /> } />
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <h5 className="text-footer">
+          Compilado por Diogo
+          { ' Sant\'' }
+          Anna
+        </h5>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
