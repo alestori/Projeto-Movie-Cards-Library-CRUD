237d6358fcc5128ac4a4c1bbb4eca491bbb7f94d
Patrick Dack <patrickdack89@gmail.com>
2021-07-07 01:28:07 -0300

    Requisito 6

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -8,19 +8,24 @@ class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      shouldRedirect: false,
+    };
   }
 
   handleSubmit(newMovie) {
-    return this.fetchAPI(newMovie);
+    this.fetchAPI(newMovie);
   }
 
-  async fetchAPI(newMovie) {
-    const request = await movieAPI.createMovie(newMovie);
-    console.log(request);
-    return <Redirect to="/" />;
+  fetchAPI(newMovie) {
+    movieAPI.createMovie(newMovie);
+    this.setState({ shouldRedirect: true });
   }
 
   render() {
+    const { shouldRedirect } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
