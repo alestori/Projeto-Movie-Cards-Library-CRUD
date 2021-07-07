c140ae5b08e652eca0b719a2a0f7d838a6613dc3
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-07 00:23:29 -0300

    inciando estilização

--- src/App.js
+++ src/App.js
@@ -7,21 +7,32 @@ import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
 import Footer from './components/Footer';
+import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/movies/new" render={ (props) => <NewMovie { ...props } /> } />
-        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
-        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-        <Route path="/:notfound" render={ (props) => <NotFound { ...props } /> } />
-        <Route path="/" render={ (props) => <MovieList { ...props } /> } />
-      </Switch>
-      <Footer />
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route path="/movies/new" render={ (props) => <NewMovie { ...props } /> } />
+            <Route
+              path="/movies/:id/edit"
+              render={ (props) => <EditMovie { ...props } /> }
+            />
+            <Route
+              path="/movies/:id"
+              render={ (props) => <MovieDetails { ...props } /> }
+            />
+            <Route path="/:notfound" render={ (props) => <NotFound { ...props } /> } />
+            <Route path="/" render={ (props) => <MovieList { ...props } /> } />
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
