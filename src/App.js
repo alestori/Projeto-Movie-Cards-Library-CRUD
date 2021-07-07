cfc4fa9048cf3425b1fcc4369b2300cfc0c749c1
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-07-06 23:10:28 -0300

    req 1 - criando rotas a partir do index.js de /pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { EditMovie, MovieDetails, NewMovie, MovieList, NotFound } from './pages';
+//  import dos components a partir do index.js em pages, mais simples.
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/" component={ MovieList } />
+        <Route path="-" component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
