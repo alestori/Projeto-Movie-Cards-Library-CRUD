043d3ecc8a75e2676fdc9eb6047e884aa1221060
Thalles Carneiro <thallescarneiro@gmail.com>
2021-07-06 21:09:40 -0300

    Adicione um link para deletar um cartão em MovieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -49,6 +49,7 @@ class MovieDetails extends Component {
         <p>{ `Rating: ${rating}` }</p>
         <div>
           <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          <Link to="/" onClick={ () => movieAPI.deleteMovie(id) }>DELETAR</Link>
           <Link to="/">VOLTAR</Link>
         </div>
       </div>
