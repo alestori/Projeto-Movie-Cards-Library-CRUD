ad1d7cb731e96c93f4ee1c39c94a9c5d20fff90b
cristianocsl <cslcristiano@gmail.com>
2021-07-06 21:00:53 -0300

    Requisitos 4: Faça uma requisição para buscar o filme que deverá ser renderizado dentro de Movie Details

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import PropType from 'prop-types';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    movieAPI.getMovie(id).then((idMovie) => this.setState({
+      movies: idMovie,
+      loading: false,
+    }));
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    // const { storyline, imagePath, genre, rating, subtitle } = {};
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movies, loading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movies;
 
+    if (loading) return <Loading />;
     return (
       <div data-testid="movie-details">
+        <Link to={ `/movies/${id}/edit` }> EDITAR </Link>
+        <Link to="/"> VOLTAR </Link>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
@@ -23,5 +46,13 @@ class MovieDetails extends Component {
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropType.shape({
+    params: PropType.shape({
+      id: PropType.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
