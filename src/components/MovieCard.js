aa584c6a26f77847ac033c9790dd9aa8e1099e6b
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-07-07 00:09:02 -0300

    req 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, id } = movie;
     return (
-      <div data-testid="movie-card">
-        Movie Card
-      </div>
+      <main>
+        <div data-testid="movie-card">
+          <h3 data-testid="movie-card-title" className="movie-card-title">{title}</h3>
+          <p className="movie-card-storyline">{storyline}</p>
+          Movie Card
+        </div>
+        <div>
+          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
+        </div>
+      </main>
     );
   }
 }
