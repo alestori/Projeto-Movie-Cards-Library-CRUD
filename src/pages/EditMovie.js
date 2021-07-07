4fb9dd652096f9348e3ba5d44e66c2b5e5a6433b
Marcela Silva <marcela.ops@hotmail.br>
2021-07-06 21:59:33 -0300

    req5 iniciando requisição para buscar filme

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
+import { MovieForm, Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: true,
+      movie: {},
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(/* updatedMovie */) {
+  // Requisito 5
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    movieAPI.getMovie(id)
+      .then((result) => this.setState({ movie: result }));
   }
+
+  handleSubmit(updatedMovie) {
+
+  }
+
+  // função updateMovie no arquivo movieAPI.js
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
+    if (shouldRedirect) return <Redirect to="/" />;
+    // Componente Redirect citado na sessão 'Habilidades' do proejto -> https://reactrouter.com/web/api/Redirect
 
-    if (status === 'loading') {
-      // render Loading
-    }
+    if (status === 'loading') return <Loading />;
 
     return (
       <div data-testid="edit-movie">
@@ -32,3 +46,11 @@ class EditMovie extends Component {
 }
 
 export default EditMovie;
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
