59b5dd974c7795e66e3466770cda936310f2c248
merlone <alestori7@gmail.com>
2021-10-03 11:13:25 -0300

    Updates

---
+++ precache-manifest.60a64fd7374b099d3b612cbc0170dfef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da074ba410e1ac60f61f01ac220ef2c",
+    "url": "/movie-cards-library-crud/index.html"
+  },
+  {
+    "revision": "ddac73a3eac26b5e92db",
+    "url": "/movie-cards-library-crud/static/css/main.af5a758c.chunk.css"
+  },
+  {
+    "revision": "fec9fe21448345a13cf7",
+    "url": "/movie-cards-library-crud/static/js/2.d276ecd7.chunk.js"
+  },
+  {
+    "revision": "ddac73a3eac26b5e92db",
+    "url": "/movie-cards-library-crud/static/js/main.e9af6c62.chunk.js"
+  },
+  {
+    "revision": "2175941fc204fe3b3689",
+    "url": "/movie-cards-library-crud/static/js/runtime-main.ac7a69e8.js"
+  },
+  {
+    "revision": "46e6f8cc0502a8cadd4d5559b625c070",
+    "url": "/movie-cards-library-crud/static/media/TheGodfather-rgRK.46e6f8cc.ttf"
+  },
+  {
+    "revision": "f8ddad071515600618abfa405be208f4",
+    "url": "/movie-cards-library-crud/static/media/felix-mooneeram-evlkOfkQ5rE-unsplash.f8ddad07.jpg"
+  },
+  {
+    "revision": "db360b27276e85748887ac97642d8a1f",
+    "url": "/movie-cards-library-crud/static/media/texgyreadventor-regular.db360b27.otf"
+  }
+]);
